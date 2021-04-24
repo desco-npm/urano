2f1dca8280729bd02d2c27c40c418158668b0241
eudiasrafael <eu@diasrafael.com.br>
2021-04-24 20:08:48 -0300

    DataCard

---
+++ components/Card/data.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  el-row.data-card
+    el-col(:span="2")
+      slot(name="icon")
+    el-col.label(:span="22")
+      slot(name="label")
+    el-col(:span="2")
+      |&nbsp;
+    el-col(:span="22")
+      slot(name="data")
+</template>
+
+<script>
+export default {
+  name: 'DataCard',
+}
+</script>
+
+<style scoped lang="scss">
+  .label {
+    font-weight: bold;
+    font-size: 1.2rem;
+    margin-top: -3px;
+  }
+</style>
