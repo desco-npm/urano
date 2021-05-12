f62d1d7482632e718cc9a8f18da1401157a27d4d
DESKTOP-7082DFH\eu <eu@diasrafael.com.br>
2021-05-12 19:25:09 -0300

    0.9.2

--- components/Input/index.vue
+++ components/Input/index.vue
@@ -1,28 +1,59 @@
 <template lang="pug">
 .ur-input
   el-input(
-    v-if="element === 'string'",
-    v-model="data",
+    v-if="element === 'string'"
+    v-model="data"
     :placeholder="attrs.placeholder"
   )
   el-input(
-    v-if="element === 'password'",
-    type="password",
-    v-model="data",
+    v-if="element === 'password'"
+    type="password"
+    v-model="data"
     :placeholder="attrs.placeholder"
   )
   el-input(
-    v-if="element === 'email'",
-    type="email",
-    v-model="data",
+    v-if="element === 'email'"
+    type="email"
+    v-model="data"
     :placeholder="attrs.placeholder"
   )
-  form-validation-error(:v="v", :name="name", :service="service")
+  el-input(
+    v-if="element === 'text'"
+    type="textarea"
+    v-model="data"
+    :placeholder="attrs.placeholder"
+  )
+  el-date-picker(
+    v-if="element === 'date'"
+    type="date"
+    v-model="data"
+    :placeholder="attrs.placeholder"
+  )
+  q-editor(
+    v-if="element === 'texteditor'"
+    v-model="data"
+    :min-height="attrs.minHeight || '15rem'"
+    :placeholder="attrs.placeholder"
+  )
+  q-file(
+    v-if="element === 'upload'"
+    filled
+    v-model="data"
+    :label="attrs.placeholder"
+    @input="fileToBase64"
+  )
+    template(v-slot:prepend)
+      q-icon(name="attach_file")
+    template(v-slot:file="{ index, file }")
+      | ola mundo
+  form-validation-error(:v="v" :name="name", :service="service")
 </template>
 
 <script>
 import ModelWatchMixin from "@desco/urano/mixins/watch/model";
 import FormValidationError from "@desco/urano/components/FormValidationError";
+
+import fileBase64 from '@desco/file-base64'
 
 export default {
   name: "UranoInput",
@@ -56,11 +87,28 @@ export default {
           return "password";
         case "email":
           return "email";
+        case "date":
+          return "date";
+        case "texteditor":
+          return "texteditor";
+        case "upload":
+          return "upload";
       }
     },
   },
+  methods: {
+    fileToBase64 () {
+      fileBase64(this.data).then(_base64 => {
+        console.log(_base64)
+      })
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+  .el-date-editor.el-input,
+  .el-date-editor.el-input__inner {
+    width: 100%;
+  }
 </style>
