37ae5970aae4ec4aacc4420c429c8bce3bed0c9b
DESKTOP-7082DFH\eu <eu@diasrafael.com.br>
2021-05-14 18:39:46 -0300

    ModelMixin

--- mixins/watch/model.js
+++ mixins/watch/model.js
@@ -5,6 +5,17 @@ export default {
     }
   },
   watch: {
+    value: {
+      deep: true,
+      handler () {
+        if (this.fetch) {
+          this.fetch(this.value)
+        }
+        else {
+          this.data = this.value
+        }
+      },
+    },
     data: {
       deep: true,
       handler() {
