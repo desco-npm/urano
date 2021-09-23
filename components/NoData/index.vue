a673eb6094b8db6414b9ac769b551a597b7e7c19
Rafael Dias <eu@diasrafael.com.br>
2021-09-23 16:01:41 -0300

    NoData

---
+++ components/NoData/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  div
+    div(v-if="data || !data.length === 0")
+      el-empty(:description="description")
+    div(v-else)
+      slot
+</template>
+
+<script>
+  export default {
+    name: 'NoData',
+    props: {
+      data: Array | null | undefined,
+      description: { type: String, default: 'Sem dados até o momento', }
+    },
+  }
+</script>
