c6f2baf0e2f5dde2271db78a39a479a6ac74da2a
Rafael Dias <eu@diasrafael.com.br>
2021-10-29 14:02:12 -0300

    geoLocation agora detecta a posição antes de disparar e nao por intervalo

--- mixins/geoLocation.js
+++ mixins/geoLocation.js
@@ -1,6 +1,6 @@
 export default {
   methods: {
-    getCurrentPosition (options = {}) {
+    getPositionGPS (options = {}) {
       return new Promise((resolve, reject) => {
         const success = p => resolve(p)
         const error = e => reject(e)
@@ -8,5 +8,8 @@ export default {
         navigator.geolocation.getCurrentPosition(success, error, options)
       })
     },
+    watchPositionGPS (onSuccess, onError = () => {}, params = {}) {
+      navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 60000, ...params, })
+    }
   },
 }
